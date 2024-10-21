d60355c406cd93f3df61f312fcb2321fd9792cb7
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2024-10-21 21:32:41 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="catto.py",
-    version="0.0.1",
+    version="0.0.2",
     author="topcatto",
     author_email="feedbackborya@yandex.ru",
     description="A Python library for making webservers.",
